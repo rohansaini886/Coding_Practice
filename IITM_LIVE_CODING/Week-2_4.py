24bd3bb9db16eb1434a4cf062b1d345b24cde781
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2022-03-16 23:20:36 +0530

    Create Week-2_4.py

---
+++ IITM_LIVE_CODING/Week-2_4.py
@@ -0,0 +1,48 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+  
+         # Finding the mid of the array
+        mid = len(arr)//2
+  
+        # Dividing the array elements
+        L = arr[:mid]
+  
+        # into 2 halves
+        R = arr[mid:]
+  
+        # Sorting the first half
+        mergeSort(L)
+  
+        # Sorting the second half
+        mergeSort(R)
+  
+        i = j = k = 0
+  
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+  
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+  
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+def subordinates(l):
+    if len(l) % 2 == 0:
+        mergeSort(l)
+        return (l, len(l) - 1)
+    else:
+        mergeSort(l)
+        return (l, len(l))
+print(subordinates(eval(input())))
